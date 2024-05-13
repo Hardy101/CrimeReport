5ab0fe90f20501fd0a64c35677d0259ed0183fbe
Eghosa Ordia <eghordia130@gmail.com>
2024-05-13 01:06:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Reporting System Documentation
+
+This reporting system is a Flask-based web application designed to allow users to report various issues and incidents. This document provides an overview of the system's functionality and how to use it.
+## Getting Started
+**Installation**: Ensure you have Python installed on your system. You can install the required dependencies using `pip`:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+**Setting Up the Database**: The Database automatically sets up when you run `app.py`
+
+**Running the Application**: Start the Flask application by running the following command in your terminal:
+
+`python app.py`
+
+**Accessing the Application**: Once the application is running, you can access it by opening a web browser and navigating to `http://localhost:5000`.
+## Functionality
+
+### Reporting Issues
+
+Users can report issues by sending a POST request to the `/api/issues` endpoint from a form. They need to provide details such as their name, victim's name, contact information, address, state, district, block, location, and the type of issue. Upon successful reporting, the issue is stored in the database, and an email notification is sent. 
+See `functions.py` for input field names.
+
+### User Registration
+
+New users can register by sending a POST request to the `/register` endpoint with their email and password. If the email is not already registered, a new user account is created.
+
+### User Login
+
+Registered users can log in by sending a POST request to the `/login` endpoint with their email and password. If the credentials are valid, the user is logged in.
+
+### Leadership Board
+
+The `/leadership` endpoint provides a list of users along with their IDs, names, and points. This allows users to view the leadership board and see the points earned by each user.
+## Endpoints
+
+- **POST `/api/issues`**: Report an issue.
+- **POST `/register`**: Register a new user.
+- **POST `/login`**: Log in as an existing user.
+- **GET `/leadership`**: View the leadership board.
+## Dependencies
+
+See `requirements.txt` 
+## Google Authentication
+
+Click on: https://developers.google.com/gmail/api/quickstart/python to get started
+
+**Step 1**: Click on `A Google cloud project` and follow instructions
+
+**Step 2**: Click on `Go to OAuth consent screen` and follow instructions
+
+**Step 2**: Click on `Go to Credentials`, follow the instructions, download `credentials.json` and save in root directory
+## Running the Application
+
+To run the application, execute the `app.py` file using Python. Make sure to set up the database before running the application.
+
---
+++ functions.py
@@ -0,0 +1,88 @@
+import geocoder
+import requests
+
+
+def User(db):
+    class UserModel(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        email = db.Column(db.String(80), unique=True, nullable=False)
+        password = db.Column(db.String(80), nullable=False)
+        point = db.Column(db.Integer)
+
+    return User
+    # Returns user object so that it can be used in adding, loggin and deleting user account
+
+
+"""
+//  Code Explanation
+--> def create_profile_db(db)
+    # Here i'm importing `db` from app.py --> db = SQLAlchemy(app)
+    The function paramter, `db` inherits everything from `db`
+
+"""
+def ReportModel(db, app):
+    class Report(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(100))
+        contact = db.Column(db.String(100))
+        victim_name = db.Column(db.String(255))
+        address = db.Column(db.String(255))
+        state = db.Column(db.String(100))
+        district = db.Column(db.String(100))
+        block = db.Column(db.String(100))
+        location = db.Column(db.String(100))
+        issue_type = db.Column(db.String(100))
+
+    with app.app_context():
+        db.create_all()
+
+    return Report
+    # Returns Report object so that it can be used in adding, and deleting record
+
+
+def send_email(r_email, issue):
+    import base64
+    from email.mime.text import MIMEText
+    from google_auth_oauthlib.flow import InstalledAppFlow
+    from googleapiclient.discovery import build
+    from requests import HTTPError
+    
+    SCOPES = [
+            "https://www.googleapis.com/auth/gmail.send"
+        ]
+    flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+    
+    creds = flow.run_local_server(port=0)
+
+    service = build('gmail', 'v1', credentials=creds)
+    
+    message = MIMEText(f"I am reporting an issue regarding {issue['issue_type']} that occurred at {issue['victim_name']}'s residence located at {issue['address']}, {issue['block']}, {issue['district']}, {issue['state']}. The incident was witnessed by {issue['name']} who can be contacted at {issue['contact']}. Thank you for your attention to this matter.")
+    message['to'] = r_email
+    message['subject'] = f'{issue['issue_type']} Report'
+    create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+    
+    try:
+        message = (service.users().messages().send(userId="me", body=create_message).execute())
+        print(F'sent message to {message} Message Id: {message["id"]}')
+    except HTTPError as error:
+        print(F'An error occurred: {error}')
+        message = None
+
+
+def location():
+    pass
+
+
+# issue = {
+#     'name': 'Petyr Baelish',
+#     'victim_name': 'Danielle',
+#     'contact': '21142',
+#     'address': '214 Kings Road',
+#     'state': 'Flee buttom',
+#     'district': 'King's landing',
+#     'block': 'Brothel Ext1',
+#     'location': 'Kings landing',
+#     'issue_type': 'Outlandish Play'
+# }
+# send_email('eseoseordia@gmail.com', issue)
