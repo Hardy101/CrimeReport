de09c83beb14bb63fc3ba74f08788fd61ff5652e
Eghosa Ordia <eghordia130@gmail.com>
2024-05-13 01:15:36 +0100

    UserModel Fix

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 def UserModel(db):
-    class UserModel(db.Model):
+    class User(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         email = db.Column(db.String(80), unique=True, nullable=False)
         password = db.Column(db.String(80), nullable=False)
